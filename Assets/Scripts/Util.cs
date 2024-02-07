743cc19f27ed71ab881b59244050d605f0d4abc9
ChocoMucho <vkclssha14@gmail.com>
2024-02-07 19:34:05 +0900

    Feat : 유틸 클래스 구현
    프로그래밍에 도움 되는 기능을 가진 정적 유틸 클래스 구현

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public static class Util
 {
+    // 프리팹 객체화 시키고 객체 반환
     public static GameObject Instantiate<T>(string path = null, Transform parent = null) where T : UIBase
     {
         if (string.IsNullOrEmpty(path))
             path = "Prefabs";
 
-        // Take Class Name
         string fileName = typeof(T).Name; 
 
-        // Take prefab
         GameObject prefab = Resources.Load<T>($"{path}/{fileName}").gameObject;
         
-        // Instantiate a prefab
         GameObject go = Object.Instantiate(prefab, parent);
         go.name = fileName;
 
         return go;
+    }
+
+    // 이름으로 자식 가져오기
+    public static T FindChild<T>(GameObject go, string componentName, bool isRecur = false) where T : UnityEngine.Object
+    {
+        if (go == null) return null;
+
+        if(!isRecur)
+        {
+            for (int i = 0; i < go.transform.childCount; ++i)
+            {
+                Transform transform = go.transform.GetChild(i);
+                if (transform.name == componentName)
+                {
+                    T component = transform.GetComponent<T>();
+                    if (component != null)
+                        return component;
+                }
+            }
+        }
+        else
+        {
+            foreach(T component in go.GetComponentsInChildren<T>())
+            {
+                if(componentName == component.name)
+                    return component;
+            }
+        }
+
+        return null;
     }
 }
